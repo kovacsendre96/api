bf390d791e4c39d344eeff526b2073c23ac9f141
Kovács Endre <kovacsendre96@gmail.com>
2021-07-05 14:28:05 +0200

    initial commit

---
+++ build/precache-manifest.e52da33ade7a70f13af5ccdc891f63d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b4da7a6a57f7935576cdaca5e922a48",
+    "url": "/api/index.html"
+  },
+  {
+    "revision": "dbb5e91d26f94d772835",
+    "url": "/api/static/css/main.442b584e.chunk.css"
+  },
+  {
+    "revision": "a8b9dea63737512022ad",
+    "url": "/api/static/js/2.03b8e3cf.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/api/static/js/2.03b8e3cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb5e91d26f94d772835",
+    "url": "/api/static/js/main.881ac25d.chunk.js"
+  },
+  {
+    "revision": "7bd405523bb169f9e801",
+    "url": "/api/static/js/runtime-main.229f6685.js"
+  }
+]);
